74b7df11cfec46b48f8db1448cfcf9dcc775ec63
yatziripineda <319132948@pcpuma.acatlan.unam.mx>
2023-05-08 14:45:15 -0500

    Primer cambio

---
+++ Proyecto_chido/Proyecto_chidoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proyecto_chidoApp.swift
+//  Proyecto_chido
+//
+//  Created by CEDAM05 on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Proyecto_chidoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
